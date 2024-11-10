c336ac7f55f698f83727c3ccf5972bcb46cba016
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-10 18:43:51 +0300

    prime checker

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,50 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main { // <tag>icerik</tag>
+public class Main {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-
-        while (testCases > 0) {
-            String line=in.nextLine();
-
-            String regex = "<(.+?)>([^<>]+)</\\1>";
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(line);
-
-            boolean found = false;
-            while (matcher.find()) {
-                System.out.println(matcher.group(2));
-                found = true;
+    static class Prime{
+        public void checkPrime(int...numbers) //birden fazla int alacak
+        {
+            boolean isFirst=true;
+            for (int num : numbers){
+                if (isPrime(num)){
+                    if (!isFirst){
+                        System.out.print(" "); //ilk sayıda boşluk olmaması için
+                    }
+                    System.out.println(num);//asalları yazdırır
+                    isFirst=false;
+                }
             }
-            if (!found) {
-                System.out.println("None");
-            }
-            testCases--;
+            System.out.println(); //metod sonrası yeni satır
         }
-        in.close();
+        private boolean isPrime(int num){
+            if (num<2){
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(num); i++){
+                if (num%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        Prime prime = new Prime();
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        int num5 = scanner.nextInt();
+
+        prime.checkPrime(num1);
+        prime.checkPrime(num1,num2);
+        prime.checkPrime(num1,num2,num3);
+        prime.checkPrime(num1,num2,num3,num4,num5);
+
+        scanner.close();
     }
 }
