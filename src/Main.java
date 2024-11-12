9cd24a45357bd0e1f81dd2f9b1a2668ba346ab48
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-12 21:18:16 +0300

    java currency formatter

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-stdin-stdout/problem?isFullScreen=true
+//https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        //for ABD
+        NumberFormat usFormat= NumberFormat.getCurrencyInstance(Locale.US);
+        String us =usFormat.format(payment);
+
+        //hindistan icin ozel olustur
+        Locale indiaLocale = new Locale("en", "IN");
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
+        String india = indiaFormat.format(payment);
+
+        //cin
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = chinaFormat.format(payment);
+
+        //Fransa
+        NumberFormat frenchFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String french = frenchFormat.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("French: " + french);
     }
 }
