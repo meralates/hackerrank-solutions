797194d08fe163a6d7b0e18ed3e799c00e888c78
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-12 21:59:21 +0300

    java big decimal

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,26 @@
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true
+
+//https://www.hackerrank.com/challenges/java-bigdecimal/problem?isFullScreen=true
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
 
-        //for ABD
-        NumberFormat usFormat= NumberFormat.getCurrencyInstance(Locale.US);
-        String us =usFormat.format(payment);
-
-        //hindistan icin ozel olustur
-        Locale indiaLocale = new Locale("en", "IN");
-        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
-        String india = indiaFormat.format(payment);
-
-        //cin
-        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        String china = chinaFormat.format(payment);
-
-        //Fransa
-        NumberFormat frenchFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        String french = frenchFormat.format(payment);
-
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("French: " + french);
+        Arrays.sort(s,0,n,(a,b)->{
+            BigDecimal bd1 = new BigDecimal(a);
+            BigDecimal bd2 = new BigDecimal(b);
+            return bd2.compareTo(bd1);
+        });
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
     }
 }
