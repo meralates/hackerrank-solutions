70175a1e1c15149aecc8640cda6160bfa108aa96
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-14 17:02:27 +0300

    java valid username regular expression

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
-//https://hackerrank.com/challenges/java-regex/problem?isFullScreen=true
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()) {
-            String IP = in.next();
-            System.out.println(IP.matches(new MyRegex().pattern));
-        }
-        in.close();
-    }
+//https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+
+class UsernameValidator {
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
 }
-class MyRegex {
-    String pattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
-                     "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+public class Main {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String username = scan.nextLine();
+
+            if (username.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            }else{
+                System.out.println("Invalid");
+            }
+        }
+    }
 }
