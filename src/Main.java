f01edc2bd5567d7441bf31259f6046663134c1ba
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-17 00:51:07 +0300

    java string reverse (palindrome)

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
-import java.time.LocalDate;
 import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-date-and-time/problem?isFullScreen=true
+//https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int month=scanner.nextInt();
-        int day=scanner.nextInt();
-        int year=scanner.nextInt();
-
-        LocalDate date=LocalDate.of(year,month,day); //belirtilen tarihi oluştur
-
-        String dayOfWeek=date.getDayOfWeek().toString();
-        System.out.println(dayOfWeek);
+        String A = scanner.nextLine();
         scanner.close();
+
+        //stringi cevirip orjinalle kıyasla
+        String reversed = new StringBuilder(A).reverse().toString();
+
+        if(A.equals(reversed)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
     }
 }
