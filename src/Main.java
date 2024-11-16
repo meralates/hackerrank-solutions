1ac77a5b70789c6aa3db7a5149305bb8734366d4
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-17 01:19:46 +0300

    java regex - duplicate words

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-string-reverse/problem?isFullScreen=true
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//https://www.hackerrank.com/challenges/duplicate-word/problem?isFullScreen=true
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String A = scanner.nextLine();
-        scanner.close();
 
-        //stringi cevirip orjinalle kıyasla
-        String reversed = new StringBuilder(A).reverse().toString();
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+"; //b kelime siniri/(\s+\1\b)tekrara bakar/(\w+) kelimeyi temsil eder
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); //buyuk kucuk harf duyarsiz esleme
 
-        if(A.equals(reversed)){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            Matcher m = p.matcher(input);
+
+            while (m.find()) {//tkr eden kelimeleri kaldirmak icin
+                input=input.replaceAll(m.group(), m.group(1));
+            }
+            System.out.println(input);
         }
+        in.close();
     }
 }
