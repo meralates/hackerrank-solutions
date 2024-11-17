bfbde1732df93bffd915a2bd14e96e41f5b25e69
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-17 23:50:22 +0300

    java anagrams

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
-//https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=true
+
+//https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=true
+//anagram=ayni harflere sahip
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
-        System.out.println(s.substring(start, end));
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+
     }
+    static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if (a.length() != b.length()) {//uzunluklar esit degilse direkt false
+            return false;
+        }
+        //stringleri arraya cevirip kiyasliyoruz
+        char[] arrA = a.toCharArray();
+        char[] arrB = b.toCharArray();
+        Arrays.sort(arrA);
+        Arrays.sort(arrB);
+        return Arrays.equals(arrA, arrB);
+    }
+
 }
