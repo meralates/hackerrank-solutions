a4279b6fe4136ba64b132f75881e55636e5121aa
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-17 23:03:27 +0300

    java substring

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-//https://www.hackerrank.com/challenges/java-primality-test/problem?isFullScreen=true
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=true
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String n = bufferedReader.readLine();
-        bufferedReader.close();
-        BigInteger number = new BigInteger(n);
-
-        if(number.isProbablePrime(10)){
-            System.out.println("prime");
-        }else{
-            System.out.println("not prime");
-        }
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(s.substring(start, end));
     }
 }
