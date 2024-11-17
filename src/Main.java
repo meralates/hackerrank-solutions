35dbd929496c60279cad1620fd40d76314275bf9
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-17 15:39:03 +0300

    java primality test

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-//https://www.hackerrank.com/challenges/duplicate-word/problem?isFullScreen=true
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+//https://www.hackerrank.com/challenges/java-primality-test/problem?isFullScreen=true
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        String regex = "\\b(\\w+)(\\s+\\1\\b)+"; //b kelime siniri/(\s+\1\b)tekrara bakar/(\w+) kelimeyi temsil eder
-        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE); //buyuk kucuk harf duyarsiz esleme
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        bufferedReader.close();
+        BigInteger number = new BigInteger(n);
 
-        Scanner in = new Scanner(System.in);
-        int numSentences = Integer.parseInt(in.nextLine());
-
-        while (numSentences-- > 0) {
-            String input = in.nextLine();
-            Matcher m = p.matcher(input);
-
-            while (m.find()) {//tkr eden kelimeleri kaldirmak icin
-                input=input.replaceAll(m.group(), m.group(1));
-            }
-            System.out.println(input);
+        if(number.isProbablePrime(10)){
+            System.out.println("prime");
+        }else{
+            System.out.println("not prime");
         }
-        in.close();
+
     }
 }
