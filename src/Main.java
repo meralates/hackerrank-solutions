69f95491a2823f547e46b21a2a70b567590c0df5
Meral ATEŞ <202103001075@dogus.edu.tr>
2024-11-19 00:20:22 +0300

    pattern syntax checker

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
-//https://www.hackerrank.com/challenges/java-anagrams/problem?isFullScreen=true
-//anagram=ayni harflere sahip
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+//
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String a = scan.next();
-        String b = scan.next();
-        scan.close();
-
-        boolean ret = isAnagram(a, b);
-        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
-
-    }
-    static boolean isAnagram(String a, String b) {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        if (a.length() != b.length()) {//uzunluklar esit degilse direkt false
-            return false;
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try{
+                Pattern.compile(pattern);
+                System.out.println("valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("invalid");
+            }
+            testCases--;
         }
-        //stringleri arraya cevirip kiyasliyoruz
-        char[] arrA = a.toCharArray();
-        char[] arrB = b.toCharArray();
-        Arrays.sort(arrA);
-        Arrays.sort(arrB);
-        return Arrays.equals(arrA, arrB);
+        in.close();
     }
-
 }
